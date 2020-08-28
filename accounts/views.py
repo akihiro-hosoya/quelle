f2b0f6386354052b5e8c2221e63eb222e3b2b92c
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-08-28 17:25:45 +0900

    ログイン機能

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from accounts.models import CustomUser
+from accounts.forms import ProfileForm
+from allauth.account import views
 
 # Create your views here.
+class LoginView(views.LoginView):
+    template_name = 'accounts/login.html'
+
 class ProfileView(View):
     def get(self, request, *args, **kwargs):
         user_data = CustomUser.objects.get(id=request.user.id)
 
         return render(request, 'accounts/profile.html', {
             'user_data': user_data,
+        })
+
+class ProfileEditView(View):
+    def get(self, request, *args, **kwargs):
+        user_data = CustomUser.objects.get(id=request.user.id)
+        form = ProfileForm(
+            request.POST or None,
+            initial = {
+                'first_name': user_data.first_name,
+                'last_name': user_data.last_name,
+                'department': user_data.department,
+                'position': user_data.position
+            }
+        )
+
+        return render(request, 'accounts/profile_edit.html', {
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            user_data = CustomUser.objects.get(id=request.user.id)
+            user_data.first_name = form.cleaned_data['first_name']
+            user_data.last_name = form.cleaned_data['last_name']
+            user_data.department = form.cleaned_data['department']
+            user_data.position = form.cleaned_data['position']
+            user_data.save()
+            return redirect('profile')
+
+        return render(request, 'accounts/profile.html', {
+            'form': form
         })
